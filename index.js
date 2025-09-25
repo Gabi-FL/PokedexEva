6b3edc208deb5655dbdb294babd2c7f9f110f1f5
Eva Otero Calleiras <evacalleiras@gmail.com>
2025-09-25 09:29:28 +0200

    Crear Pokedex

---
+++ index.js
@@ -0,0 +1,16 @@
+const pokemon = "Leafeon";
+const statsList = document.getElementById("stats");
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  .then((response) => response.json())
+  .then((data) => {
+    const stats = data.stats;
+    stats.forEach((stat) => {
+      const statLi = document.createElement("li");
+      statLi.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+      statsList.appendChild(statLi);
+    });
+    const tipos = data.types;
+    tipos.forEach((tipo))
+  })
+  .catch((error) => console.error("Error al obtener los datos:", error));
